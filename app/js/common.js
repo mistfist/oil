31431027051281bc7bdc9c3ff1bb46cd55000501
mistfist <vitokud1@gmail.com>
2018-02-21 00:24:33 +0200

    mobile-version

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,35 @@
 $(function() {
 
-	// Custom JS
+	//E-mail Ajax Send
+	$("#mail-form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо, Ваше сообщение отправлено!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
+		$('#slide-submenu').on('click',function() {			        
+			$(this).closest('.list-group').fadeOut('slide',function(){
+				$('.mini-submenu').fadeIn();	
+			});
+			
+		  });
+	
+		$('.mini-submenu').on('click',function(){		
+			$(this).next('.list-group').toggle('slide');
+			$('.mini-submenu').hide();
+		})
+
+	
 
 });
