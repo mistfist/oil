0cb47bbf628a5a57383893a2ace4539b82adb074
mistfist <vitokud1@gmail.com>
2018-02-23 01:08:31 +0200

    test-mobile

--- app/js/common.js
+++ app/js/common.js
@@ -17,19 +17,37 @@ $(function() {
 		return false;
 	});
 
+//back to top
+$(document).ready(function(){
+    $(function () {
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+});
 
-		$('#slide-submenu').on('click',function() {			        
-			$(this).closest('.list-group').fadeOut('slide',function(){
-				$('.mini-submenu').fadeIn();	
-			});
-			
-		  });
-	
-		$('.mini-submenu').on('click',function(){		
-			$(this).next('.list-group').toggle('slide');
-			$('.mini-submenu').hide();
-		})
-
-	
+$(window).scroll(function(){ 
+  if($(window).scrollTop()>600){ 
+    $('#back-top').show() 
+  } else 
+   { $('#back-top').hide() };
+});
 
 });
+
+
+
+function initMap() { 
+	var uluru = {lat: 46.960717, lng: 31.980674};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 15,
+	  center: uluru
+	});
+	var marker = new google.maps.Marker({
+	  position: uluru,
+	  map: map
+	});
+  }
